c70626c16a547fc7340282dd7e713f4ef30d4ac5
Farid AMOUSSA <amousssa.farid2@gmail.com>
2021-07-13 22:29:10 +0100

    Issues 64 resolved edit, user leave team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,11 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  def edit
+    unless current_user == @team.owner
+      redirect_to team_url(params[:id]), notice: "You are not authorized to do this action"
+    end
+  end
 
   def create
     @team = Team.new(team_params)
